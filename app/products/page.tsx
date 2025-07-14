7e0eece98e691936e3f90b2cf7e2985ab0fb8d07
kb603 <kojoaddobaffour@gmail.com>
2025-07-14 15:45:18 +0000

    updates

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,3 +1,14 @@
-export default function ProductsPage() {
-  return <div>Products Page</div>;
+import ProductList from "@/components/ProductList";
+import { stripe } from "@/lib/stripe";
+
+export default async function ProductsPage() {
+  const products = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
+  return (
+    <div>
+      <h1>All Products</h1>
+      <ProductList products={products.data} />
+    </div>
+  );
 }
---
+++ components/ProductList.tsx
@@ -0,0 +1,29 @@
+import Stripe from "stripe";
+import ProductCard from "./ProductCard";
+
+interface Props {
+  products: Stripe.Product[];
+}
+
+export default function ProductList({ products }: Props) {
+  return (
+    <div>
+      <div className="mb-6 flex justify-center">
+        <input
+          type="text"
+          placeholder="Search products..."
+          className="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+      </div>
+      <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        {products.map((product, key) => {
+          return (
+            <li key={key}>
+              <ProductCard product={product} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}

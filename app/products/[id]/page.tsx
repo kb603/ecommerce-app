9046810c4b265d8deb861b7c559a1379302e3d35
kb603 <kojoaddobaffour@gmail.com>
2025-07-15 20:12:50 +0000

    update

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,14 @@
+import ProductDetail from "@/components/ProductDetail";
+import { stripe } from "@/lib/stripe";
+
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const product = await stripe.products.retrieve(id, {
+    expand: ["default_price"],
+  });
+  return <ProductDetail product={product} />;
+}
